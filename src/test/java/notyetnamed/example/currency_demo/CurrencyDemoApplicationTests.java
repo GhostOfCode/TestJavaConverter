406c29117a830a4fd6047d8d5d8b3f235ec91463
Paladin <ghostofdeath2023@gmail.com>
2020-07-29 21:34:37 +0300

    Initial commit

---
+++ src/test/java/notyetnamed/example/currency_demo/CurrencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package notyetnamed.example.currency_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
